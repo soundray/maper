cbde075af66fe46f8b0d04c7a0163d2a0e70cf95
Linus Heckemann <git@sphalerite.org>
2019-12-29 17:24:22 +0100

    example: fix curl command

--- run-maper-example-generate.sh
+++ run-maper-example-generate.sh
@@ -99,7 +99,7 @@ if [[ ! -e \$atlas.tar ]] ; then
     dlcommand="wget -O -"
     url=https://github.com/soundray/maper/releases/download/0.9.0-rc/\$atlas.tar
     # url=https://soundray.org/maper/\$atlas.tar
-    type wget >/dev/null 2>&1 || dlcommand="curl --output -"
+    type wget >/dev/null 2>&1 || dlcommand="curl -fL --output -"
     \$dlcommand \$url >\$atlas.tar || fatal "Download failed. wget or curl must be installed"
 fi
 [[ ! -e \$atlas.tar ]] && fatal "No tarfile found -- something went wrong"
