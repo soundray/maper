a98867fe2805f18f4ca29f5bf179a7524765e970
Linus Heckemann <git@sphalerite.org>
2024-05-28 15:38:30 +0200

    docker: update nixpkgs, make /etc writable to work around singularity bug

--- docker.nix
+++ docker.nix
@@ -1,14 +1,23 @@
 {
-  # nixos-19.09 as of 2019-12-29
-  pkgsPath ? builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/eab4ee0c27c5c6f622aa0ca55091c394a9e33edd.tar.gz
+  # nixos-unstable as of 2023-05-26
+  pkgsPath ? builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/bfb7a882678e518398ce9a31a881538679f6f092.tar.gz
 , pkgs ? import <nixpkgs> {}
 }:
 let
   maper = pkgs.callPackage ./default.nix {};
+  env = pkgs.buildEnv {
+    name = "maper-docker-env";
+    paths = [ maper pkgs.bashInteractive pkgs.coreutils pkgs.cacert ];
+  };
   # docs: https://nixos.org/nixpkgs/manual/#sec-pkgs-dockerTools
-in pkgs.dockerTools.buildImage {
-  name = "maper";
+in (pkgs.dockerTools.buildImage {
+  name = "registry.oak.sphalerite.org/maper";
   tag = "latest";
-  contents = [ maper pkgs.bashInteractive pkgs.coreutils pkgs.cacert pkgs.vim ];
+  copyToRoot = env;
   config.Env = [ "NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt" ];
+  runAsRoot = ''
+    chmod -vR u+w /etc
+  '';
+}) // {
+  inherit env;
 }
